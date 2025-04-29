02c25e69125da7fd9edd74ad77f64e150b6030d9
rafly <raflyadityaray3@gmail.com>
2025-04-29 16:13:52 +0700

    langsung login

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\MerkController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::resource('barang', BarangController::class);
-Route::resource('merk', MerkController::class);
+
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('/', HomeController::class);
+    Route::resource('barang', BarangController::class);
+    Route::resource('merk', MerkController::class);
+});
